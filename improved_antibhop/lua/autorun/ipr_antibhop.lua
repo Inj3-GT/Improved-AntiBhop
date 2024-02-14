57da7ec2bd8b5fa3dc2b0b9e3518a38b35fa805b
Inj3 <58576616+Inj3-GT@users.noreply.github.com>
2024-02-14 02:23:40 +0100

    v1.3

--- improved_antibhop/lua/autorun/ipr_antibhop.lua
+++ improved_antibhop/lua/autorun/ipr_antibhop.lua
@@ -10,21 +10,22 @@ local ipr_disabled_autojump = false
 local ipr_key = IN_JUMP
 local ipr_movetype = MOVETYPE_NOCLIP
 local ipr_bhop = {}
+local ipr_cmd = 0
 
 hook.Add("StartCommand", "Ipr_Anti_BhopRestrict", function(ply, cmd)
     if not cmd:KeyDown(ipr_key) then
         return
     end
 
-    if (cmd:CommandNumber() ~= 0) then
+    if (cmd:CommandNumber() ~= ipr_cmd) then
         if not IsValid(ply) or not ply:Alive() or (ply:GetMoveType() == ipr_movetype) then
             return
         end
-        local ipr_cur = CurTime()
         if not ipr_bhop[ply] then
             ipr_bhop[ply] = {}
         end
-        
+            
+        local ipr_cur = CurTime()   
         if (ipr_bhop[ply].pushc) then
             if (ipr_cur < ipr_bhop[ply].nextc) then
                 cmd:RemoveKey(ipr_key)
